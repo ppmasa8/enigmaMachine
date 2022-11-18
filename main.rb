835e40cab56d293062c32001128b1175ed48bec5
ppmasa8 <kiyokiyopip56@gmail.com>
2022-11-18 11:41:16 +0900

    Added rotor class

--- main.rb
+++ main.rb
@@ -1,10 +1,27 @@
 require "./plug_board"
+require "./rotor"
 
 def main
-    plug_board = PlugBoard.new("BADC")
-    encrypted_idx = plug_board.forward(ALPHABET.index("A"))
+    # CHECK PlugBoard class
+    #
+    # plug_board = PlugBoard.new("BADC")
+    # encrypted_idx = plug_board.forward(ALPHABET.index("A"))
+    # p ALPHABET[encrypted_idx]
+    # decrepted = ALPHABET[plug_board.backward(encrypted_idx)]
+    # p decrepted
+
+    # CHECK Rotor class
+    rotor = Rotor.new("BADC", 1)
+    encrypted_idx = rotor.forward(ALPHABET.index("A"))
     p ALPHABET[encrypted_idx]
-    decrepted = ALPHABET[plug_board.backward(encrypted_idx)]
+    decrepted = ALPHABET[rotor.backward(encrypted_idx)]
+    p decrepted
+
+    rotor.rotate()
+
+    encrypted_idx = rotor.forward(ALPHABET.index("A"))
+    p ALPHABET[encrypted_idx]
+    decrepted = ALPHABET[rotor.backward(encrypted_idx)]
     p decrepted
 end
 
---
+++ rotor.rb
@@ -0,0 +1,21 @@
+class Rotor < PlugBoard
+    attr_accessor :offset, :rotations
+
+    def initialize(map_alphabet, offset=0)
+        super(map_alphabet)
+        @offset = offset
+        @rotations = 0
+    end
+
+    def rotate(offset=nil)
+        offset = self.offset unless offset
+        self.alphabet = self.alphabet[offset..] + self.alphabet[..offset]
+        self.rotations += offset
+        self.rotations
+    end
+
+    def reset
+        self.rotations = 0
+        self.alphabet = ALPHABET
+    end
+end
