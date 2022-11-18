b07d2ed9d1b1540800b717c5c883e3d6d035be33
ppmasa8 <kiyokiyopip56@gmail.com>
2022-11-18 11:17:52 +0900

    Added forward and backward function

--- enigma.rb
+++ enigma.rb
@@ -14,4 +14,17 @@ class PlugBoard
         self.forward_map = Hash[alphabet.zip(map_alphabet)].compact
         self.backward_map = forward_map.invert
     end
+
+    def forward(idx)
+        char = self.alphabet[idx]
+        char = self.forward_map[char]
+        self.alphabet.index(char)
+    end
+
+    def backward(idx)
+        char = self.alphabet[idx]
+        char = self.backward_map[char]
+        self.alphabet.index(char)
+    end
+    
 end
--- main.rb
+++ main.rb
@@ -2,8 +2,10 @@ require "./enigma"
 
 def main
     plug_board = PlugBoard.new("BADC")
-    p plug_board.forward_map
-    p plug_board.backward_map
+    encrypted_idx = plug_board.forward(ALPHABET.index("A"))
+    p ALPHABET[encrypted_idx]
+    decrepted = ALPHABET[plug_board.backward(encrypted_idx)]
+    p decrepted
 end
 
 main()
